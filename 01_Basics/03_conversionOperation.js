8062842d0312fd0732151dedb79f50b77bb02616
Keshav Pal <106235495+Keshavpal22@users.noreply.github.com>
2024-10-01 10:08:44 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score= false
+
+// console.log(typeof score)
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn = "hllo"
+
+let boleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boleanIsLoggedIn)
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
